188c97207a6a783df307db1ca4ee7149160212e2
unh6unh6 <ruddbs3308@naver.com>
2024-12-09 20:05:44 +0900

    feat(InputView): 경주 시도 횟수 입력 안내 문구를 출력한다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,15 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_NAME_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL_INPUT_GUIDE_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public void printInputGuideMessage() {
-        System.out.println(INPUT_GUIDE_MESSAGE);
+    public void printCarNameInputGuideMessage() {
+        System.out.println(CAR_NAME_INPUT_GUIDE_MESSAGE);
+    }
+
+    public void printTrialInputGuideMessage() {
+        System.out.println(TRIAL_INPUT_GUIDE_MESSAGE);
     }
 
     public String getInput() {
